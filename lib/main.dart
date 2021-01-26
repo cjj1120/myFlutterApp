d3157c2670c09f59b19d998ddd77c3ec5fe8ad89
cjj1120 <cjj8168@gmail.com>
2021-01-26 22:07:42 +0800

    done video 3.3

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Flutter wow'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I AM On the state of FLOW'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://ec.europa.eu/programmes/creative-europe/sites/default/files/covid19-cdc-unsplash.jpg'),
+          ),
+        ),
       ),
     ),
   );
